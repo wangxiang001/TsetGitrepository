c190d89924a8fc80ac904364a2405da52b0f7d15
wang xiang <1271468141@qq.com>
2020-07-24 19:55:49 +0800

    第二次提交

--- src/main/java/git/Test.java
+++ src/main/java/git/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("这是我的第一个git项目");
         System.out.println("李易峰你好！");
+        System.out.println("明天你好！又是充满希望的一天！");
     }
 }
